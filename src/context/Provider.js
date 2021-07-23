b85c4d295d8474fb5390faf591e4c346d0ae954f
guinhoanjos <iago.anjos@hotmail.com>
2021-07-23 15:20:15 -0300

    hooks no provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,49 +1,26 @@
 // src/context/Provider.js
 
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-  }
-
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
-    });
+function Provider({children}) {
+  const [cars, setCars] = useState({})
+ function moveCar(car, side) {
+   console.log(cars)
+   setCars({...cars, [car]:side})
   };
-
-  render() {
     const context = {
-      ...this.state,
-      moveCar: this.moveCar,
+      cars,
+      moveCar,
     };
 
-    const { children } = this.props;
+
 
     return (
       <CarsContext.Provider value={context}>
         {children}
       </CarsContext.Provider>
     );
-  }
-};
-
-Provider.propTypes = {
-  children: PropTypes.node.isRequired,
 };
 
 export default Provider;
